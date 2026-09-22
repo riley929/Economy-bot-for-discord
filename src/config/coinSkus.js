3e2722890d2932983466d2b4f3ff68865a2b740c
riley929 <81786302+riley929@users.noreply.github.com>
2026-09-22 03:49:51 +0100

    Refactor coinSkus.js to use empty strings for keys
    Removed specific APP_ID and SKU keys, replacing them with empty strings.

--- src/config/coinSkus.js
+++ src/config/coinSkus.js
@@ -1,38 +1,38 @@
 module.exports = {
-  APP_ID: "1497016445744189520",
+  APP_ID: "",
 
   COIN_SKUS: {
-    "1506846151855575142": {
+    "": {
       name: "10,000 Clover Coins",
       coins: 10_000,
       price: "$1.99",
     },
 
-    "1506846178480754778": {
+    "": {
       name: "20,000 Clover Coins",
       coins: 20_000,
       price: "$3.99",
     },
 
-    "1506846201431982190": {
+    "": {
       name: "30,000 Clover Coins",
       coins: 30_000,
       price: "$5.99",
     },
 
-    "1506846237339422720": {
+    "": {
       name: "40,000 Clover Coins",
       coins: 40_000,
       price: "$7.99",
     },
 
-    "1506846282470391888": {
+    "": {
       name: "50,000 Clover Coins",
       coins: 50_000,
       price: "$9.99",
     },
 
-    "1506846329761042452": {
+    "": {
       name: "100,000 Clover Coins",
       coins: 100_000,
       price: "$19.99",
